f90ad86479a6cb3b963292406848647e68642feb
Bart Langelaan <langelaanbart@gmail.com>
2016-08-17 02:28:56 +0200

    Add simple front-end table with all servers and sites

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,10 @@
 var router = require('express').Router();
 var servers = require('../functions/servers');
 
-router.get('/servers', function(req, res){
-    res.json(servers.servers.map(server => server.toJSON()));
+router.get('/', function(req, res){
+    res.json({
+        servers: servers.servers.map(server => server.toJSON())
+    });
 });
 
 module.exports = router;
