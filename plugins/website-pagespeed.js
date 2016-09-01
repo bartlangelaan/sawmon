003611cfcc18e5fd2f444ae5aa930961606e62a3
Bart Langelaan <langelaanbart@gmail.com>
2016-09-01 18:07:06 +0200

    Add PageSpeed results

---
+++ plugins/website-pagespeed.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const request = require('request-promise');
+
+module.exports.schema = {
+    pageSpeedScore: Number
+};
+
+module.exports.ping = website => {
+    if(website.status != 200) return;
+
+    return request('https://www.googleapis.com/pagespeedonline/v2/runPagespeed', {
+        qs: {
+            url: 'http://' + website.domain
+        },
+        json: true
+    }).then(result => {
+        if(result && result.ruleGroups && result.ruleGroups.SPEED && result.ruleGroups.SPEED.score){
+            website.pageSpeedScore = result.ruleGroups.SPEED.score;
+            website.save();
+        }
+    }).catch(error => {
+        // Let it go
+    });
+};
