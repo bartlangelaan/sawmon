f485096e138531590aa5d70d8be3d80017ca634d
Bart Langelaan <langelaanbart@gmail.com>
2016-09-08 10:54:09 +0200

    Add front-end for server-adding

--- public/src/javascript/index.js
+++ public/src/javascript/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var $ = require('jquery');
 require('expose?$!expose?jQuery!jquery');
 require('bootstrap/dist/js/bootstrap.js');
@@ -6,14 +8,14 @@ require('datatables.net-bs');
 
 
 $.getJSON('api/websites').then(function(data){
-    $("#websites").DataTable(Object.assign(data, {
+    $('#websites').DataTable(Object.assign(data, {
         ajax: {
             url: 'api/websites'
         }
     }));
 });
 $.getJSON('api/servers').then(function(data){
-    $("#servers").DataTable(Object.assign(data, {
+    $('#servers').DataTable(Object.assign(data, {
         ajax: {
             url: 'api/servers'
         }
@@ -24,8 +26,57 @@ $.getJSON('api/servers').then(function(data){
  * App
  */
 function refresh() {
-    $("#websites").DataTable().ajax.reload(null, false);
-    $("#servers").DataTable().ajax.reload(null, false);
+    $('#websites').DataTable().ajax.reload(null, false);
+    $('#servers').DataTable().ajax.reload(null, false);
 }
 
 setInterval(refresh, 1000);
+
+$('#newServer').submit(function(){
+    var $this = $(this);
+    var $alert = $this.find('.alert');
+
+    /**
+     * Hide the error alert
+     */
+    $alert.hide();
+
+    /**
+     * Get all data
+     */
+    var data = $this.serializeArray().reduce(function(obj, item) {
+        obj[item.name] = item.value;
+        return obj;
+    }, {});
+
+    /**
+     * Do the request
+     */
+    $.ajax({
+        data: data,
+        dataType: 'json',
+        method: $this.attr('method'),
+        url: $this.attr('action')
+    }).done(function(){
+
+        /**
+         * When successful, close model and reset form
+         */
+        $this.closest('.modal').one('hidden.bs.modal', function(){
+            $this[0].reset();
+        }).modal('hide');
+
+    }).fail(function(data){
+
+        /**
+         * When unsuccessful, show the error alert
+         */
+        $alert.text(data.responseJSON.error).show();
+
+    });
+
+    /**
+     * Prevent form from submitting
+     */
+    return false;
+});
